d369ae24feb32b667a799b5f1d6ec645db1e49fe
aylalah <ayoadelala@yahoo.com>
2018-10-15 18:37:39 +0100

    change to Navbar-defualt

--- src/components/guestNav.vue
+++ src/components/guestNav.vue
@@ -1,65 +1,52 @@
 
 <template>
   <div id="app">
- <!-- Navbar -->
-  <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-default scrolling-navbar">
     <div class="container">
 
-      <!-- Brand -->
-      <a class="navbar-brand waves-effect" href="https://mdbootstrap.com/material-design-for-bootstrap/" target="_blank">
-        <strong class="blue-text">MDB</strong>
+      <a class="navbar-brand" href="#" target="_blank">
+        <strong>SoFine</strong>
       </a>
 
-      <!-- Collapse -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- Links -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <!-- Left -->
         <ul class="navbar-nav mr-auto">
+         
           <li class="nav-item active">
-            <a class="nav-link waves-effect" href="#">Home
+            <a  class="nav-link" href="#" v-on:click="side('home')" >Home
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link waves-effect" href="https://mdbootstrap.com/material-design-for-bootstrap/" target="_blank">About MDB</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link waves-effect" href="https://mdbootstrap.com/getting-started/" target="_blank">Free download</a>
+            <a class="nav-link" href="" target="_blank">Policy</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link waves-effect" href="https://mdbootstrap.com/bootstrap-tutorial/" target="_blank">Free tutorials</a>
+            <a class="nav-link" href="" target="_blank">Support</a>
           </li>
         </ul>
 
-        <!-- Right -->
         <ul class="navbar-nav nav-flex-icons">
           <li class="nav-item">
-            <a class="nav-link waves-effect">
-              <span class="badge red z-depth-1 mr-1"> 1 </span>
-              <i class="fa fa-shopping-cart"></i>
-              <span class="clearfix d-none d-sm-inline-block"> Cart </span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.facebook.com/mdbootstrap" class="nav-link waves-effect" target="_blank">
+            <a href="#" class="nav-link" target="_blank">
               <i class="fa fa-facebook"></i>
             </a>
           </li>
           <li class="nav-item">
-            <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
+            <a href="#" class="nav-link" target="_blank">
               <i class="fa fa-twitter"></i>
             </a>
           </li>
           <li class="nav-item">
-            <a href="https://github.com/mdbootstrap/bootstrap-material-design" class="nav-link border border-light rounded waves-effect"
-              target="_blank">
-              <i class="fa fa-github mr-2"></i>MDB GitHub
+          <a href="#" class="nav-link border border-light rounded"  v-on:click="side('sign')" >
+              <i></i>sign in
             </a>
           </li>
         </ul>
@@ -69,6 +56,7 @@
     </div>
   </nav>
   <!-- Navbar -->
+
       <div v-if="homes">
           <Home></Home>
       </div>
@@ -179,6 +167,7 @@
     <!--/.Copyright-->
 
   </footer>
+
   </div>
 </template>
 
@@ -207,6 +196,7 @@ export default {
    this.view = (show == 'home')?this.homes=true:this.homes=false;
    this.view = (show == 'sign')?this.signIns=true:this.signIns=false;
   }
+
   },
   components: {
     Home,
