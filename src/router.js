4762ac53eac16a78986d4256b7afac1d9df179f7
Olarinde <delightolarinde@gmail.com>
2018-10-24 15:01:33 +0100

    tailor side upgraded

--- src/router.js
+++ src/router.js
@@ -7,6 +7,16 @@ import GuestNav from './components/guestNav.vue'
 import HomeDashboard from './components/homeDashboard.vue'
 import ProductDashboard from './components/productDashboard.vue'
 import ProductPage from './components/product-page.vue'
+import TailorDashboard from './components/tailor-dashboard.vue'
+import Detail from './components/details.vue'
+import Manage from './components/manage.vue'
+import MyService from './components/myService.vue'
+import MySupport from './components/mySupport.vue'
+import MyProfile from './components/myProfile.vue'
+import Earnings from './components/Myearning.vue'
+import Setting from './components/setting.vue'
+import ManageJob from './components/manageJob.vue'
+import Client from './components/client.vue'
 
 vue.use(Router)
 
@@ -46,6 +56,57 @@ routes: [
 		path: '/productPage',
 		name: 'productPage',
 		component: ProductPage
+	},
+	{
+		path: '/tailorhome',
+		name: 'HomePage',
+		component:TailorDashboard
+	},
+	{
+		path: '/myService',
+		name: 'myService',
+		component: MyService
+	},
+	{
+		path: '/mySupport',
+		name: 'mySupport',
+		component: MySupport
+	},
+
+	{
+		path: '/myprofile',
+		name: 'myprofile',
+		component: MyProfile
+	},
+	{
+		path: '/setting',
+		name: 'setting',
+		component: Setting
+	},
+	{
+		path: '/manageJob',
+		name: 'manageJob',
+		component: ManageJob
+	},
+	{
+		path: '/earn',
+		name: 'earn',
+		component: Earnings
+	},
+	{
+		path: '/client',
+		name: 'clients',
+		component: Client
+	},
+	{
+		path: '/details',
+		name: 'details',
+		component: Detail
+	},
+	{
+		path: '/manage',
+		name: 'manage',
+		component: Manage
 	}
 ]
 	})
